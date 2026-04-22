da37694b1908f109270909a081185eab137851b2
Vanhle05 <Vanhlenorthside@gmail.com>
2026-04-22 21:12:04 +0700

    debug: point api/index.js back to server directory

--- api/index.js
+++ api/index.js
@@ -1,8 +1,1 @@
-const express = require('express');
-const app = express();
-
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok from bare api/index.js' });
-});
-
-module.exports = app;
+module.exports = require('../server/index');
